1e79792ad74ca177a9d29f497c171d29863dd8da
mcglaser <michaelcharlesglaser@gmail.com>
2017-07-15 16:44:41 -0700

    fixed address parsing through google api

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -19,19 +19,25 @@ class DistrictsController < ApplicationController
   
   def parse_address
     @street = params[:Street]
-    response = HTTParty.get("https://www.googleapis.com/civicinfo/v2/representatives?key=#{ENV["MAPS_API_KEY"]}&address=#{@street}")
+    response = HTTParty.get("https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyDAScfx_c-PTavuWMLSGRsWSQDTpCeW1Cg&address=#{@street}")
     @json = JSON.parse(response.body)
    
 #Determines Congressional District 
     @division_hash = @json.fetch("divisions")
     @ocd_division = @division_hash.keys[2] 
     @district_hash = @division_hash.fetch(@ocd_division)
+#    @district_name = @district_hash.fetch("name").titleize
 
 #Creates Hash Of All Office Holders In District    
     @office_holders_hash = @json.fetch("officials")
     
-### US Senator One
+# US Senator One
     @senator_one_hash = @office_holders_hash[2]
+ #   @senator_one_name = @senator_one_hash.fetch("name")
+ #   @senator_one_party = @senator_one_hash.fetch("party")
+ #   @senator_one_photo_url = @senator_one_hash.fetch("photoUrl")
+    @senator_one_websites_array = @senator_one_hash.fetch("urls")
+ #   @senator_one_website = @senator_one_websites_array[0]
 
     #US Senator One Social Media
     @senator_one_socials_array = @senator_one_hash.fetch('channels', nil)
@@ -51,9 +57,15 @@ class DistrictsController < ApplicationController
     end
 
 
-### US Senator Two
+# US Senator Two
     @senator_two_hash = @office_holders_hash[3]
+#    @senator_two_name = @senator_two_hash.fetch("name")
+#    @senator_two_party = @senator_two_hash.fetch("party")
+#    @senator_two_photo_url = @senator_two_hash.fetch("photoUrl")
     @senator_two_websites_array = @senator_two_hash.fetch("urls")
+#    @senator_two_website = @senator_two_websites_array[0]
+
+    #US Senator Two Social Media
     @senator_two_socials_array = @senator_two_hash.fetch('channels', nil)
     
     
@@ -70,9 +82,15 @@ class DistrictsController < ApplicationController
       end
     end
 
-### US Rep 
+# US Rep 
     @us_rep_hash = @office_holders_hash[4]
+#    @us_rep_name = @us_rep_hash.fetch("name")
+#    @us_rep_party = @us_rep_hash.fetch("party")
+#    @us_rep_photo_url = @us_rep_hash.fetch("photoUrl")
     @us_rep_websites_array = @us_rep_hash.fetch("urls")
+#    @us_rep_website = @us_rep_websites_array[0]
+
+    #US Rep Social Media
     @us_rep_socials_array = @us_rep_hash.fetch('channels', nil)
     
     
@@ -116,6 +134,7 @@ class DistrictsController < ApplicationController
 
 
       @district.save
+ #     @district
 
       redirect_to district_path(@district)
   
